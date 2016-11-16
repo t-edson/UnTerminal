00ea0831c940d4dce5af3c13371c87bdfc735dc7
Tito Hinostroza <dev_null_1@hotmail.com>
2016-11-16 10:13:13 -0500

    Add files via upload

--- Example2/unit1.pas
+++ Example2/unit1.pas
@@ -71,7 +71,7 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   proc:= TConsoleProc.Create(nil);
-  proc.LineDelim := TTL_CRLF;
+  proc.LineDelimSend := LDS_CRLF;
   proc.OnLineCompleted:=@procLineCompleted;
   proc.OnReadData:=@procReadData;
   proc.OnChangeState:=@procChangeState;
